0fd45c0d9086d03345ec4f757b6ecfe6ccde7d27
alfaiz khan <84037292+alfaizk64@users.noreply.github.com>
2024-05-29 22:12:47 +0530

    Add files via upload

---
+++ Day3-practice/script.js
@@ -0,0 +1,139 @@
+// Arthimetic Operator
+
+
+let a = 10;
+let b =20;
+
+document.write( "a + b = ", a+b );
+document.write("<br/>")
+document.write("<br/>")
+
+
+let c = 30;
+let d = 10;
+document.write("b - a = ", b-a );
+document.write("<br/>")
+document.write("<br/>")
+
+let e = 5;
+let f = 10;
+document.write("e * f = ", e*f );
+document.write("<br/>")
+document.write("<br/>")
+
+
+let g = 10;
+let h = 10;
+document.write("e / f = ", g/h );
+document.write("<br/>")
+document.write("<br/>")
+
+
+let i = 2;
+let j = 10;
+document.write("e % f = ", g%h );
+document.write("<br/>")
+document.write("<br/>")
+
+// Comparison Operator
+
+document.write("Comparison operator")
+document.write("<br/>")
+document.write("<br/>")
+
+
+
+
+let x=10;
+let y=10;
+document.write(" x==y ", x==y , " The Number X is Equal to y");
+document.write("<br/>")
+document.write("<br/>")
+
+
+
+let z=10;
+let q=7;
+document.write(" x!=y ", z!=q , " The Number z is not Equal to q");
+document.write("<br/>")
+document.write("<br/>")
+
+
+let u=10;
+let v=80;
+
+document.write(" u>=v ", u>=v, " u is less than v ");
+document.write("<br/>")
+document.write("<br/>")
+document.write("u <= v ", u<=v , " u is less than v");
+
+// Logical Operator
+
+document.write("<br/>")
+document.write("<br/>")
+document.write("<br/>")
+
+document.write("Logical Operator")
+document.write("<br/>")
+document.write("<br/>")
+
+
+
+
+
+let p = 90;
+let m = 60;
+
+document.write(" p>10 && m>10 ", p>10 && m>10 ," both the number greater than  10" );
+document.write("<br/>")
+document.write("<br/>")
+
+
+// even or not
+
+document.write("p%2 || m%2 ", p%2 || m%2," both the number is even");
+document.write("<br/>")
+document.write("<br/>")
+
+
+// negative no
+
+
+document.write("<br/>")
+document.write("<br/>")
+
+document.write("Assingment Operator")
+document.write("<br/>")
+document.write("<br/>")
+
+
+let n=5;
+document.write(n)
+
+document.write("<br/>")
+document.write("<br/>")
+
+let num =10;
+ num +=5;
+ document.write(num + " updated value ");
+ document.write("<br/>")
+ document.write("<br/>")
+
+ let num1=3;
+
+ num1*=2
+document.write(num1 + " updated value")
+document.write("<br/>")
+document.write("<br/>")
+
+
+document.write("Ternary Operator")
+document.write("<br/>")
+document.write("<br/>")
+
+num >= 0 ? document.write(" Number is Positive") : document.write(" Number isNegative");
+
+
+
+
+
